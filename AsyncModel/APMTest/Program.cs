0389c45b6326a400d701a0c0d7bc819463b1ae03
wacky <344609124@qq.com>
2018-07-19 12:00:16 +0800

    Add files via upload

---
+++ AsyncModel/APMTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace APMTest
+{
+    class Program
+    {
+        public delegate void PrintDelegate();
+
+        static void Main(string[] args)
+        {
+            Async();
+            Console.WriteLine("主线程执行完成");
+            Console.Read();
+        }
+
+        private static void Async()
+        {
+            PrintDelegate del = new PrintDelegate(Print);
+            IAsyncResult ar = del.BeginInvoke(new AsyncCallback(CallBack), null);
+        }
+
+        private static void Print()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("执行完成！");
+        }
+
+        private static void CallBack(IAsyncResult ar)
+        {
+            while (true) 
+            {
+                if (ar.IsCompleted) 
+                {
+                    Console.WriteLine("执行完成回调");
+                    break;
+                }
+            }
+            Console.WriteLine("回调完成");
+        }
+
+    }
+}
---
+++ AsyncModel/TPLTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TPLTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task<int> task = new Task<int>(() => Add(cts.Token), cts.Token);
+            task.Start();
+            task.ContinueWith(TaskEnded);
+            Console.ReadKey();
+            cts.Cancel();
+            Console.WriteLine(task.Result);
+            Console.ReadKey();
+        }
+
+        static void TaskEnded(Task<int> task)
+        {
+            Console.WriteLine("任务完成，完成时候的状态为：");
+            Console.WriteLine("IsCanceled={0}\tIsCompleted={1}\tIsFaulted={2}", task.IsCanceled, task.IsCompleted, task.IsFaulted);
+            Console.WriteLine("任务的返回值为：{0}", task.Result);
+        }
+
+        static int Add(CancellationToken ct)
+        {
+            Console.WriteLine("任务开始...");
+            int result = 0;
+            try
+            {
+                while (true)
+                {
+                    ct.ThrowIfCancellationRequested();
+                    result++;
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+
+            }
+            return result;
+        }
+    }
+}
